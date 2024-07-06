8908f23d53529a8f60f9fda646d67c664478432d
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-07-06 09:45:18 +0200

    ZombieAnt Behaviour added
    Infection is now posible

---
+++ Assets/Ants/Scripts/ZombieAnt.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ZombieAnt : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+    private Transform workerAntContainer; 
+
+    private GameObject targetWorkerAnt;
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        workerAntContainer = GameManager.instance.workAntsContainer;
+    }
+
+    void Update()
+    {
+        if (targetWorkerAnt == null)
+        {
+            FindNewWorkerAnt();
+        }
+
+        if (targetWorkerAnt != null)
+        {
+            agent.SetDestination(targetWorkerAnt.transform.position);
+        }
+
+        UpdateAnimation();
+    }
+
+    void UpdateAnimation()
+    {
+        animator.SetBool("isWalking", agent.velocity.magnitude > 0.1f);
+    }
+
+    void FindNewWorkerAnt()
+    {
+        WorkerAnt[] workerAnts = FindObjectsOfType<WorkerAnt>();
+
+        if (workerAnts.Length > 0)
+        {
+            int randomIndex = Random.Range(0, workerAnts.Length);
+            targetWorkerAnt = workerAnts[randomIndex].gameObject;
+        }
+        else
+        {
+            agent.SetDestination(transform.position);  
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("WorkerAnt"))
+        {
+            if (targetWorkerAnt != null && other.gameObject == targetWorkerAnt)
+            {
+                InfectWorkerAnt(other.gameObject);
+            }
+        }
+    }
+
+    void InfectWorkerAnt(GameObject workerAnt)
+    {
+
+
+        Destroy(workerAnt);
+
+        GameObject newZombieAnt = Instantiate(gameObject, workerAnt.transform.position, Quaternion.identity);
+        
+        if (GameManager.instance.zombiesAntContainer != null)
+        {
+            newZombieAnt.transform.parent = GameManager.instance.zombiesAntContainer;
+        }
+    }
+}
